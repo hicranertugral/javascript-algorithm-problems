373a3a21d2e6e3b9a3828f08eb4796d4448d8d76
hicranertugral <hicranertugral@gmail.com>
2024-04-02 15:20:26 +0300

    Function that calculates the radius of the circle. B

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 //1- Dairenin yarıçapını hesaplayan fonksiyon.
 //1- Function that calculates the radius of the circle.
 
-function calculateRadius(area) {
+// Answer A
+function calculateRadiusA(area) {
   const pi = Math.PI;
   let radius = Math.sqrt(area / pi);
   return radius;
 }
 
-let area = 78.5;
-console.log(calculateRadius(area));
+let area = 80;
+console.log(calculateRadiusA(area));
+
+// Answer B
+function calculateRadiusB(area){
+    const pi = 3.14159
+    let radius = (area / pi) ** 0.5;
+    return radius
+}
+console.log(calculateRadiusB(area));
